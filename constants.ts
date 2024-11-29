b6fa51ea410d861367b586b51d34c25c66793198
Uchida.S <uchida.s@cp-info.co.jp>
2024-11-29 19:20:24 +0900

    リソースタイプの文言を修正

--- constants.ts
+++ constants.ts
@@ -10,8 +10,8 @@ export type ResourceDict = {
 export const messageDict: {[K: string]: string;} = {
     'resourcetype:ec2:instance': 'EC2インスタンス',
     'resourcetype:ec2:instance_en': 'EC2 Instance',
-    'resourcetype:rds:db-instance': 'RDSインスタンス',
-    'resourcetype:rds:db-instance_en': 'RDS Instance',
+    'resourcetype:rds:db': 'RDSインスタンス',
+    'resourcetype:rds:db_en': 'RDS Instance',
 };
 
 export const ignoreTags: string[] = [
